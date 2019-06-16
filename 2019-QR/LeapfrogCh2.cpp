72121b06547f560509e4d8ba5ce1973d2453b80a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-06-15 20:14:37 -0700

    submit FHC 2019 QR B. Leapfrog: Ch. 2

---
+++ 2019-QR/LeapfrogCh2.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+/*
+ 
+ 6/14/2019
+ 
+ 20:06-20:30 pause
+ 
+ 6/15/2019
+ 
+ 19:50-20:12 submit
+ 
+ */
+
+// $ g++ -std=c++11 -Wall -O2 -D_GLIBCXX_DEBUG LeapfrogCh2.cpp && ./a.out < LeapfrogCh2.in | diff LeapfrogCh2.out -
+string S;
+int N;
+
+bool solve() {
+  N=SZ(S);
+  map<char,int> cnt;
+  REP(i,N) cnt[S[i]]++;
+  string res;
+  if(cnt['.']==0) return false;
+  if(cnt['B']*2>=N-1) return true;
+  if(cnt['B']<=1) return false;
+  return true;
+}
+int main(int argc, char* argv[]) {
+  int T; cin>>T;
+  for(int t=1; t<=T; ++t) {
+    cin>>S;
+    
+    cout<<"Case #"<<t<<": ";
+    cout<<(solve()?"Y":"N")<<endl;
+  }
+  return 0;
+}
